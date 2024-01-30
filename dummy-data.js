683a8fa02ccfeadccc2095dc18ba4dc67eefd8f9
panpan <panpan31415@gmail.com>
2024-01-30 16:10:42 +0100

    add some images

--- dummy-data.js
+++ dummy-data.js
@@ -16,7 +16,7 @@ const DUMMY_EVENTS = [
       "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
     location: "New Wall Street 5, 98765 New Work",
     date: "2021-05-30",
-    image: "images/introvert-event.jpg",
+    image: "images/introvert-event.avif",
     isFeatured: true,
   },
   {
@@ -26,7 +26,7 @@ const DUMMY_EVENTS = [
       "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
     location: "My Street 12, 10115 Broke City",
     date: "2022-04-10",
-    image: "images/extrovert-event.jpg",
+    image: "images/extrovert-event.avif",
     isFeatured: true,
   },
 ];
